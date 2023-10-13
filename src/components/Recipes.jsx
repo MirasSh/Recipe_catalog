626f633d453e1b0149264e0d453f7c004f986a9a
MirasSh <miras.anisa@gmail.com>
2023-10-13 19:03:39 +0600

    End

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,7 +1,41 @@
 import '../App.css'
-import { Link, NavLink, useNavigate, } from 'react-router-dom';
+import { useState, useEffect } from "react";
+
 
 const About = () => {
+    const [todos, setTodos] = useState(() => {
+        const storedTodos = localStorage.getItem("todos");
+        return storedTodos ? JSON.parse(storedTodos) : [];
+    }); 
+    const [inputValue, setInputValue] = useState("");
+
+    function handleInputChange(event) {
+        setInputValue(event.target.value);
+    } 
+
+    function handleAddTodo() {
+        if (inputValue) {
+            setTodos([
+                ...todos,
+                {
+                    text: inputValue,
+                    id: Math.random().toString(36).substring(7),
+                    checked: false 
+                },
+            ]);
+            setInputValue("");
+        }
+    }
+
+    function handleDelete(index) {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    } 
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
     return (
         <>
             <body>
@@ -19,10 +53,10 @@ const About = () => {
                                 <div className="col-lg-8 col-md-10 order-md-2 order-3">
                                     <nav className="header__menu">
                                         <ul>
-                                            <li><a href="/">Home</a></li>
-                                            <li className="active"><a href="Recipes">Recipes</a></li>
-                                            <li><a href="About">About</a></li>
-                                            <li><a href="Contact">Contact</a></li>
+                                            <li><a href="/">Главная</a></li>
+                                            <li className="active"><a href="Recipes">Рецепты</a></li>
+                                            <li><a href="About">Обо мне</a></li>
+                                            <li><a href="Contact">Контакты</a></li>
                                         </ul>
                                     </nav>
                                 </div>
@@ -36,12 +70,25 @@ const About = () => {
                     </div>
                     <div className="container">
                         <div className="row">
-                            <div className="col-lg-3 col-md col-md-2-3">
+                            <div className="col-lg-3 col-md-3">
                             </div>
-                            <div className="col-lg-6 col-md col-md-2-6">
+                            <div className="col-lg-6 col-md-6">
                                 <div className="header__logo">
                                     <a href="/"><img src="logo.png" alt="" /></a>
                                 </div>
+                                <div className='flex'>
+                                <input
+                                    className="input_style"
+                                    type="text"
+                                    placeholder="Введите рецепт"
+                                    value={inputValue}
+                                    onChange={handleInputChange}
+                                />{" "}
+                                <button className="kettik" onClick={handleAddTodo}>
+                                    Добавить
+                                </button>
+                                </div>
+                               
                             </div>
                         </div>
                     </div>
@@ -52,110 +99,112 @@ const About = () => {
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="breadcrumb__text">
-                                        <h2>About me</h2>
-                                        <div className="breadcrumb__option">
-                                            <a href="#">Home</a>
-                                            <span>About</span>
-                                        </div>
+                                        <h2>Рецепты</h2>
                                     </div>
                                 </div>
                             </div>
+
+
+
                             <div className="row">
                                 <div className="col-lg-6">
                                     <div className="about__pic__item__large">
                                         <img src="about/about-1.jpg" alt="" />
                                     </div>
-                                    <div className="about__pic">
-                                        <div className="about__pic__item">
-                                            <img src="about/about-2.jpg" alt="" />
-                                        </div>
-                                        <div className="about__pic__item">
-                                            <img src="about/about-3.jpg" alt="" />
-                                        </div>
-                                    </div>
                                 </div>
                                 <div className="col-lg-6">
                                     <div className="about__right__text">
-                                        <h2>Hello every one !!!</h2>
+                                        <h2>Пхали из моркови и тыквы</h2>
                                         <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                            nisi ut aliquip ex ea commodo consequat. Duis aute irure Lorem ipsum dolor sit amet, consectetur
-                                            adipisicing elit, incididunt ut labore et dolore magna aliqua accusantium doloremque
-                                            laudantium.
-                                        </p>
-                                        <ul>
-                                            <li>Class aptent taciti sociosqu ad litora torquent per conubia nostra</li>
-                                            <li>Inceptos himenaeos mauris.Integer gravida tincidunt accumsan.</li>
-                                            <li>Vestibulum nulla mauris, condimentum id felis ac, volutpat volutpat mi.</li>
-                                            <li>In vitae tempor velit of the impenetrable foliage.</li>
-                                        </ul>
-                                        <p>
-                                            Vestibulum commodo nulla eu augue tincidunt rutrum. Suspendisse interdum lacus in ligula
-                                            finibus luctus. Vivamus mollis libero vel orci finibus, sit amet malesuada lectus aliquam. In
-                                            auctor viverra eros. Maecenas elit mi, consectetur nec, sollicitudin sed arcu. Curabitur tempor
-                                            tempor pharetra ridiculus mus porta tincidunt, purus enim laoreet.
+                                            Продукты <br />
+                                            Морковь — 150 г в очищенном виде<br />
+                                            Тыква — 300 г в очищенном виде<br />
+                                            Орехи грецкие (ядра) — 90 г<br />
+                                            Лук репчатый — 50 г<br />
+                                            Масло растительное — 50 мл<br />
+                                            Кинза свежая — 15 г<br />
+                                            Чеснок — 15 г<br />
+                                            Розмарин сушёный — 1 ч. ложка<br />
+                                            Перец чёрный молотый — по вкусу<br />
+                                            Клюква свежая или вяленая (для украшения) — 9 шт.<br />
+                                            Соль — по вкусу<br />
+                                            Хмели-сунели — 2 щепотки<br />
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </section>
-                <footer className="footer">
-                    <div className="container-fluid">
-                        <div className="footer__instagram">
-                            <div className="row">
-                                <div className="col-lg-2 col-md-2 col-sm-4 col-6">
-                                    <div className="footer__instagram__item set-bg-19" data-setbg="footer/ip-1.jpg"></div>
-                                </div>
-                                <div className="col-lg-2 col-md-2 col-sm-4 col-6">
-                                    <div className="footer__instagram__item set-bg-20" data-setbg="footer/ip-2.jpg"></div>
-                                </div>
-                                <div className="col-lg-2 col-md-2 col-sm-4 col-6">
-                                    <div className="footer__instagram__item set-bg-21" data-setbg="footer/ip-3.jpg"></div>
-                                </div>
-                                <div className="col-lg-2 col-md-2 col-sm-4 col-6">
-                                    <div className="footer__instagram__item set-bg-22" data-setbg="footer/ip-4.jpg"></div>
-                                </div>
-                                <div className="col-lg-2 col-md-2 col-sm-4 col-6">
-                                    <div className="footer__instagram__item set-bg-23" data-setbg="footer/ip-5.jpg"></div>
-                                </div>
-                                <div className="col-lg-2 col-md-2 col-sm-4 col-6">
-                                    <div className="footer__instagram__item set-bg-24" data-setbg="footer/ip-6.jpg"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="footer__text">
-                                    <div className="footer__logo">
-                                        <a href="#"><img src="logo.png" alt="" /></a>
+                            <hr />
+                            {todos.map((todo, index) => (
+                                <div key={todo.id} className="row">
+                                    <div className="col-lg-6">
+                                        <div className="about__pic__item__large">
+                                            <img src="about/about-1.jpg" alt="" />
+                                        </div>
                                     </div>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                        ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus
-                                        commodo viverra maecenas accumsan lacus vel facilisis.
-                                    </p>
+                                    <div className="col-lg-6">
+                                        <div className="about__right__text">
+                                            <h2>{todo.text}</h2>
+                                            <button onClick={() => handleDelete(index)}>Удалить</button>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-                <div className="search-model">
-                    <div className="h-100 d-flex align-items-center justify-content-center">
-                        <div className="search-close-switch">+</div>
-                        <form className="search-model-form">
-                            <input type="text" id="search-input" placeholder="Search here....." />
-                        </form>
+                            ))}
                     </div>
                 </div>
-            </body>
+            </section>
+            <footer className="footer">
+                <div className="container-fluid">
+                    <div className="footer__instagram">
+                        <div className="row">
+                            <div className="col-lg-2 col-md-2 col-sm-4 col-6">
+                                <div className="footer__instagram__item set-bg-19" data-setbg="footer/ip-1.jpg"></div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-4 col-6">
+                                <div className="footer__instagram__item set-bg-20" data-setbg="footer/ip-2.jpg"></div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-4 col-6">
+                                <div className="footer__instagram__item set-bg-21" data-setbg="footer/ip-3.jpg"></div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-4 col-6">
+                                <div className="footer__instagram__item set-bg-22" data-setbg="footer/ip-4.jpg"></div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-4 col-6">
+                                <div className="footer__instagram__item set-bg-23" data-setbg="footer/ip-5.jpg"></div>
+                            </div>
+                            <div className="col-lg-2 col-md-2 col-sm-4 col-6">
+                                <div className="footer__instagram__item set-bg-24" data-setbg="footer/ip-6.jpg"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="footer__text">
+                                <div className="footer__logo">
+                                    <a href="#"><img src="logo.png" alt="" /></a>
+                                </div>
+                                <p>
+                                Соблюдение принципов здорового питания под силу каждому. Прочитайте приведенные ниже правила и возьмите их за основу при планировании своего питания — и первый шаг к здоровому питанию и образу жизни будет сделан.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <div className="search-model">
+                <div className="h-100 d-flex align-items-center justify-content-center">
+                    <div className="search-close-switch">+</div>
+                    <form className="search-model-form">
+                        <input type="text" id="search-input" placeholder="Search here....." />
+                    </form>
+                </div>
+            </div>
+        </body >
         </>
-    )
-}
+
+    );
+};
 export default About;
 
 
